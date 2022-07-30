88e3baff613861198bb2bbeb482adfe418eba353
Sebastian Fix <sebastian.fix@codebar.ch>
2022-07-30 16:41:43 +0200

    Icons

--- tests/IconClassTest.php
+++ tests/IconClassTest.php
@@ -2,7 +2,56 @@
 
 use CodebarAg\LaravelDefault\Facades\Icon;
 
-it('icon:', function () {
-    $hey = Icon::heroicons();
-    expect(true)->toBe(false);
+
+it('icon: list icons', function () {
+    $icons = Icon::list();
+
+    expect($icons)
+        ->toBeCollection()
+        ->not()->toBeEmpty();
+
+    $icon = $icons->first();
+
+    expect($icon)
+        ->toBeArray()
+        ->toHaveKeys(['name', 'collection']);
+
+})->group('icons');
+
+it('icon: list heroicons', function () {
+    $icons = Icon::heroicons();
+    expect($icons)
+        ->toBeCollection()
+        ->not()->toBeEmpty();
+
+    $icon = $icons->first();
+
+    expect($icon)
+        ->toBeArray()
+        ->toHaveKeys(['name', 'collection']);
+
+    $icon = $icons->first();
+
+    expect($icon['name'])
+        ->toBeString()
+        ->and($icon['collection'])
+        ->toBeString()
+        ->toEqual('heroicons');
+
+})->group('icons');
+
+it('icon: list fontawesome', function () {
+    $icons = Icon::fontawesome();
+    expect($icons)
+        ->toBeCollection()
+        ->not()->toBeEmpty();
+
+    $icon = $icons->first();
+
+    expect($icon['name'])
+        ->toBeString()
+        ->and($icon['collection'])
+        ->toBeString()
+        ->toEqual('fontawesome');
+
 })->group('icons');
