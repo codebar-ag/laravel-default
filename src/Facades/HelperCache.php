38ee380c702feae9eb964719ead68bb7bea5ba68
Sebastian Fix <sebastian.fix@codebar.ch>
2022-09-25 17:50:53 +0200

    Fixed CacheHelper

--- src/Facades/HelperCache.php
+++ src/Facades/HelperCache.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \CodebarAg\LaravelDefault\Helpers\HelperCache
  *
- * @method static int cacheForeverWithTags(array $tags, string $key, $store)
- * @method static int cacheRememberWithTags(array $tags, string $key, int $time, $store)
- * @method static int flushTags(array $tags)
+ * @method static cacheForeverWithTags(array $tags, string $key, $store)
+ * @method static cacheRememberWithTags(array $tags, string $key, int $time, $store)
+ * @method static bool flushTags(array $tags)
  * @method static int inSeconds()
  */
 class HelperCache extends Facade
