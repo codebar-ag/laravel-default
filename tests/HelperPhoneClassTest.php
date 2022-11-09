f7f597e2146191412f7366db7fb6d3c5798d3fb4
Sebastian Fix <sebastian.fix@codebar.ch>
2022-11-09 22:01:02 +0100

    WIP

---
+++ tests/HelperPhoneClassTest.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodebarAg\LaravelDefault\Facades\HelperPhone;
+
+it('helper phone: format', function () {
+    $number = '+41791234567';
+    $coutry = 'CHE';
+    $formatted = HelperPhone::format($coutry,$number);
+    expect($formatted)->toBe("+41 (0) 79 123 45 67");
+})->group('phone');
