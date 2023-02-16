f3f025306a52f2ca34b06dec2f62e3e13997028b
Sebastian Fix <sebastian.fix@codebar.ch>
2023-02-16 11:08:54 +0100

    WIP

---
+++ src/Checks/Default.php
@@ -0,0 +1,15 @@
+<php
+
+//        Health::checks([
+//            DebugModeCheck::new(),
+//            EnvironmentCheck::new(),
+//            SecurityAdvisoriesCheck::new(),
+//            RedisCheck::new(),
+//            // RedisMemoryUsageCheck::new()->failWhenAboveMb(1000),
+//            // QueueCheck::new(),
+//            CacheCheck::new(),
+//            FailedJobs::new(),
+//            HorizonCheck::new(),
+//            ScheduleCheck::new(),
+//            OptimizedAppCheck::new(),
+//        ]);
