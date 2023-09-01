f1d5ad4e820dabb49028d785d9ac8c92dc773b6d
Sebastian Fix <sebastian.fix@codebar.ch>
2023-09-01 08:58:44 +0200

    WIP

--- src/Checks/JobsCheck.php
+++ src/Checks/JobsCheck.php
@@ -8,17 +8,28 @@ use Spatie\Health\Checks\Result;
 
 class JobsCheck extends Check
 {
-    public function run(): Result
-    {
-        $count = DB::table('jobs')->count();
+	public function run(): Result
+	{
+		$count = DB::table('jobs')->count();
 
-        $result = Result::make();
-        $result->shortSummary("jobs table count: {$count}");
+		$result = Result::make();
+		$result->shortSummary("jobs table count: {$count}");
 
-        if ($count > 0) {
-            return $result->failed();
-        }
+		if ($count <= 0) {
+			return $result->ok();
+		}
 
-        return $result->ok();
-    }
+		sleep(15);
+
+		$newCount = DB::table('jobs')->count();
+
+		$result->shortSummary("jobs table count: {$newCount}");
+
+		if ($count != $newCount) {
+			return $result->ok();
+		}
+
+		return $result->failed();
+
+	}
 }
