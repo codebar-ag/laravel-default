78fa10ea5640ede17749843d82cf58befed079f2
StanBarrows <StanBarrows@users.noreply.github.com>
2024-01-10 11:27:35 +0000

    Fix styling

--- src/Checks/JobsCheck.php
+++ src/Checks/JobsCheck.php
@@ -8,28 +8,28 @@ use Spatie\Health\Checks\Result;
 
 class JobsCheck extends Check
 {
-	public function run(): Result
-	{
-		$count = DB::table('jobs')->count();
+    public function run(): Result
+    {
+        $count = DB::table('jobs')->count();
 
-		$result = Result::make();
-		$result->shortSummary("jobs table count: {$count}");
+        $result = Result::make();
+        $result->shortSummary("jobs table count: {$count}");
 
-		if ($count <= 0) {
-			return $result->ok();
-		}
+        if ($count <= 0) {
+            return $result->ok();
+        }
 
-		sleep(15);
+        sleep(15);
 
-		$newCount = DB::table('jobs')->count();
+        $newCount = DB::table('jobs')->count();
 
-		$result->shortSummary("jobs table count: {$newCount}");
+        $result->shortSummary("jobs table count: {$newCount}");
 
-		if ($count != $newCount) {
-			return $result->ok();
-		}
+        if ($count != $newCount) {
+            return $result->ok();
+        }
 
-		return $result->failed();
+        return $result->failed();
 
-	}
+    }
 }
